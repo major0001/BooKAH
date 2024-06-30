413d814a2db9863b8078fa6f34246a7a0dde2931
manojsai4545 <Mnaramre@stevens.edu>
2024-07-01 02:06:29 +0300

    portfolio project

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System (BooKAH)|<a href="https://github.com/major0001" target="_blank"  > Designed by : Alex Wambua</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
