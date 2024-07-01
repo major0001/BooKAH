c2ad254773d8aaac39b6bff8008f8701ab3c6ec6
major0001 <alexmutune746@gmail.com1>
2024-07-02 02:02:12 +0300

    debuggingb bookah

---
+++ BooKAH/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System (BooKAH)|<a href="https://github.com/major0001/BooKAH" target="_blank"  > Designed by : Alex Wambua</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
